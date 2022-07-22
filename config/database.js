7f41dff761c47ba16960de4a76671499c7a1bcad
Victor Mwendwa <83487428+Vic-The-Developer@users.noreply.github.com>
2022-07-22 18:37:30 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: 'mongodb://Admin:VictorMwendwa@victech-media-shard-00-00.lhlgr.mongodb.net:27017,victech-media-shard-00-01.lhlgr.mongodb.net:27017,victech-media-shard-00-02.lhlgr.mongodb.net:27017/transport-management?ssl=true&replicaSet=atlas-ksor6c-shard-0&authSource=admin&retryWrites=true&w=majority'
+    database: ''
 }
